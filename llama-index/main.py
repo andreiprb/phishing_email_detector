6a7a8313dab8fe9d67d69dce59f169ed6186f7f4
dan <dan.ignat@student.unitbv.ro>
2025-04-22 16:39:14 +0300

    metadata added

--- llama-index/main.py
+++ llama-index/main.py
@@ -1,9 +1,20 @@
+from typing import Any
 import pandas as pd
 
 from SpamDetector import SpamDetector
 
+def get_metadata(df: pd.DataFrame) -> dict[str, Any]:
+    metadata = {
+        'sender': df.get('sender', '')[:100],
+        'date': df.get('date', '')[:30],
+        'subject': df.get('subject', '')[:200],
+        'label': df.get('label', '')[:50]
+    }
+
+    return metadata
+
 if __name__ == "__main__":
-    df = pd.read_csv("../data/datasets/Nazario.csv")
+    df: pd.DataFrame = pd.read_csv("../data/datasets/Nazario.csv")
     sample = df.sample(n=1)
     sample_body = sample["body"].iloc[0]
     print(f"Sample body: {sample_body}\n\n")
@@ -14,7 +25,7 @@ if __name__ == "__main__":
         top_k=2
     )
 
-    classification, confidence = detector.classify(sample_body)
+    classification, confidence = detector.classify(sample_body, metadata=get_metadata(sample),)
     print(f"Classification: {classification}")
     print(f"Confidence: {confidence:.2f}")
     print(f"Actual answer: {"spam" if sample['label'].item() == 1 else "ham"}")
